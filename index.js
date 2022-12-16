b570d3373d694a376634fe41c6e231ca6504b4a5
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-12-16 14:38:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+function hasTargetSum(array,target) {
+  // Write your algorithm her
+  let size = array.length
+
+  for (i = 0; i < (size - 1); i++) {
+
+    for (j = (i + 1); j < size; j++) {
+
+      if (array[i] + array[j] == target) {
+
+        return true;
+
+      }
+      
+    }
+  }
+
+  return false;
+}
+
+
+/* 
+  Write the Big O time complexity of your function here
+*/
+
+/* 
+  Add your pseudocode here
+*/
+
+/*
+  Add written explanation of your solution here
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+}
+
+module.exports = hasTargetSum;
